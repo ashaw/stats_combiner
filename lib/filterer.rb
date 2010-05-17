74aeecd5025eedae17fb57dfe23e440de029a37f
Al Shaw <al@tpm-imac>
2010-05-17 16:52:21 -0400

    detab

--- lib/filterer.rb
+++ lib/filterer.rb
@@ -1,107 +1,107 @@
 module StatsCombiner
 
-	class Filterer 
-		
-		# Initialize a filters object
-		# e = Filters.new
-		#
-		def initialize()
-			@filters ||= []
-			#clear
-		end 
-		
-		
-		# Add a filter that StatsCombiner can use to manipulate paths and titles it
-		# gets from Chartbeat.
-		# 
-		# options:                  Pattern: http://[prefix].[host]/[path][suffix]
-		#
-		#  prefix => nil            Filter on a prefix
-		#  suffix => nil            Filter on a suffix
-		#  title_regex => nil       Filter on a title pattern
-		#  path_regex=> nil         Filter on a path pattern
-		#
-		#  modify_title => true     Modify the title inline via the regex
-		#  modify_path => true      Modify the path inline via the regex
-		#  append_to_path => true   Append suffix to path rather than modifying it
-		#  exclude => true          Exclude this pattern from the top ten list
-		def add(options={})   
-			{ :prefix => nil,
-				:suffix => nil,
-				:title_regex => nil,
-				:path_regex => nil,
-				:modify_title => false, 
-				:exclude => false,
-			}.merge!(options)
-			
-			filter = {}
-			filter[:rule] = {}.merge!(options)
-						
-			@filters << filter
-		end
-	
-		
-		def list_filters
-			@filters.each do |filter|
-				filter
-				p filter[:rule]
-			end
-		end
-		
-		# a datum comes in from chartbeat data, and is manipulated
-		# with the apply_filters method
-		#
-		# Internal Usage:
-		#  f.apply_filters({
-		#   :title => Title
-		#   :path => path
-		#  })
-		#
-		def apply_filters!(datum={})
-			
-			datum = { 
-				:title => nil,
-				:path => nil,
-				:prefix => nil
-			}.merge!(datum)
-			
-			@filters.each do |filter|
-	
-							
-				if filter[:rule][:prefix] && filter[:rule][:title_regex]
-					if datum[:title].match(filter[:rule][:title_regex])
-						datum[:prefix] = filter[:rule][:prefix]
-					end
-				end
-				
-				if filter[:rule][:suffix] && filter[:rule][:path_regex]
-					if datum[:path].match(filter[:rule][:path_regex])
-						if filter[:rule][:modify_path]
-							datum[:path].gsub!(filter[:rule][:path_regex],filter[:rule][:suffix])
-						elsif filter[:rule][:append_to_path]
-							datum[:path] = datum[:path] + filter[:rule][:suffix]       
-						end
-					end
-				end
-	
-				if filter[:rule][:modify_title]
-					datum[:title].gsub!(filter[:rule][:title_regex], '')
-					datum[:title].strip!
-				end
-		
-				if datum[:prefix].nil?
-					datum[:prefix] = 'www'
-				end
-				
-			
-			end
-		
-			datum
-		
-		end
-	
-	
-	end
+  class Filterer 
+    
+    # Initialize a filters object
+    # e = Filters.new
+    #
+    def initialize()
+      @filters ||= []
+      #clear
+    end 
+    
+    
+    # Add a filter that StatsCombiner can use to manipulate paths and titles it
+    # gets from Chartbeat.
+    # 
+    # options:                  Pattern: http://[prefix].[host]/[path][suffix]
+    #
+    #  prefix => nil            Filter on a prefix
+    #  suffix => nil            Filter on a suffix
+    #  title_regex => nil       Filter on a title pattern
+    #  path_regex=> nil         Filter on a path pattern
+    #
+    #  modify_title => true     Modify the title inline via the regex
+    #  modify_path => true      Modify the path inline via the regex
+    #  append_to_path => true   Append suffix to path rather than modifying it
+    #  exclude => true          Exclude this pattern from the top ten list
+    def add(options={})   
+      { :prefix => nil,
+        :suffix => nil,
+        :title_regex => nil,
+        :path_regex => nil,
+        :modify_title => false, 
+        :exclude => false,
+      }.merge!(options)
+      
+      filter = {}
+      filter[:rule] = {}.merge!(options)
+            
+      @filters << filter
+    end
+  
+    
+    def list_filters
+      @filters.each do |filter|
+        filter
+        p filter[:rule]
+      end
+    end
+    
+    # a datum comes in from chartbeat data, and is manipulated
+    # with the apply_filters method
+    #
+    # Internal Usage:
+    #  f.apply_filters({
+    #   :title => Title
+    #   :path => path
+    #  })
+    #
+    def apply_filters!(datum={})
+      
+      datum = { 
+        :title => nil,
+        :path => nil,
+        :prefix => nil
+      }.merge!(datum)
+      
+      @filters.each do |filter|
+  
+              
+        if filter[:rule][:prefix] && filter[:rule][:title_regex]
+          if datum[:title].match(filter[:rule][:title_regex])
+            datum[:prefix] = filter[:rule][:prefix]
+          end
+        end
+        
+        if filter[:rule][:suffix] && filter[:rule][:path_regex]
+          if datum[:path].match(filter[:rule][:path_regex])
+            if filter[:rule][:modify_path]
+              datum[:path].gsub!(filter[:rule][:path_regex],filter[:rule][:suffix])
+            elsif filter[:rule][:append_to_path]
+              datum[:path] = datum[:path] + filter[:rule][:suffix]       
+            end
+          end
+        end
+  
+        if filter[:rule][:modify_title]
+          datum[:title].gsub!(filter[:rule][:title_regex], '')
+          datum[:title].strip!
+        end
+    
+        if datum[:prefix].nil?
+          datum[:prefix] = 'www'
+        end
+        
+      
+      end
+    
+      datum
+    
+    end
+  
+  
+  end
 
 end
 
